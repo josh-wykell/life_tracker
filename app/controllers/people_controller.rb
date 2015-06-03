5748745dc4f1ccee2cb34ef5686e16240bb760ca
Josh Wykell <jwykell@gmail.com>
2015-06-03 18:33:49 -0400

    people working

--- app/controllers/people_controller.rb
+++ app/controllers/people_controller.rb
@@ -2,16 +2,16 @@ class PeopleController < ApplicationController
   before_action :set_person, only: [:show, :edit, :update, :destroy]
 
   def index
-    @person = Person.all
+    @people = Person.all
   end
 
   def show
-    @person.school
-    @person.life_events
+    # @person.school
+    # @person.life_events
   end
 
   def create
-    @person = Person.new(person_params)
+    @person = Person.create(person_params)
     redirect_to @person, notice: 'New Person Created'
   end
 
@@ -20,13 +20,17 @@ class PeopleController < ApplicationController
     redirect_to @person, notice: 'Person Deleted'
   end
 
+  def new
+    @person = Person.new
+  end
+
   private
 
     def set_person
       @person = Person.find(params[:id])
     end
 
-    def school_params
+    def person_params
       params.require(:person).permit(:name)
     end
 
